92fcef75505889bf65c34d9a8061cf39b816366a
Tyler Drabek <tjdrabek@gmail.com>
2023-12-06 15:32:50 -0700

    add ts for props

--- src/components/FeedbackItem.tsx
+++ src/components/FeedbackItem.tsx
@@ -1,6 +1,18 @@
 import { TriangleUpIcon } from "@radix-ui/react-icons";
 
-export default function FeedbackItem({ feedbackItem }) {
+type FeedbackItem = {
+  upvoteCount: number;
+  badgeLetter: string;
+  companyName: string;
+  feedbackText: string;
+  daysAgo: number;
+};
+
+type FeedbackItemProps = {
+  feedbackItem: FeedbackItem;
+};
+
+export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
   return (
     <li className="feedback">
       <button>
@@ -8,7 +20,7 @@ export default function FeedbackItem({ feedbackItem }) {
         <span>{feedbackItem.upvoteCount}</span>
       </button>
       <div>
-        <p>{feedbackItem.badgetLetter}</p>
+        <p>{feedbackItem.badgeLetter}</p>
       </div>
       <div>
         <p>{feedbackItem.companyName}</p>
