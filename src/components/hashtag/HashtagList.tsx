dec7d1f094298a7d50e5adc355595c785cf330bf
Tyler Drabek <tjdrabek@gmail.com>
2023-12-07 16:50:06 -0700

    add console log to show how Hashtag list re-renders unnecessarily

--- src/components/hashtag/HashtagList.tsx
+++ src/components/hashtag/HashtagList.tsx
@@ -2,6 +2,7 @@ import { useFeedbackItemsContext } from "../../lib/hooks";
 import HashtagItem from "./HashtagItem";
 
 export default function HashtagList() {
+  console.log("HashtagList rendering...");
   const { companyList, handleSelectCompany } = useFeedbackItemsContext();
   return (
     <ul className="hashtags">
