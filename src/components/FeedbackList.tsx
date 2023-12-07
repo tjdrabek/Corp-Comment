79d14a9d85f75ac8f693182fc5043ce29ee53ca6
Tyler Drabek <tjdrabek@gmail.com>
2023-12-06 22:04:15 -0700

    handle data fetching errors

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,24 +1,36 @@
 import FeedbackItem from "./FeedbackItem";
 import { useEffect, useState } from "react";
 import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
   useEffect(() => {
     setIsLoading(true);
     fetch(
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
     )
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) throw new Error();
+        return res.json();
+      })
       .then((data) => {
         setFeedbackItems(data.feedbacks);
+      })
+      .catch(() => {
+        // network error, not 2xx response, or JSON parsing error
+        setErrorMessage("Something went wrong.");
+      })
+      .finally(() => {
         setIsLoading(false);
       });
   }, []);
   return (
     <ol className="feedback-list">
       {isLoading ? <Spinner /> : null}
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
       {feedbackItems.map((feedbackItem) => (
         <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
