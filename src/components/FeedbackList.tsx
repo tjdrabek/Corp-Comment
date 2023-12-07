0d000c9e7754ccb429713452aad45eaff5406697
Tyler Drabek <tjdrabek@gmail.com>
2023-12-06 22:14:47 -0700

    convert fetch to async await

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -8,24 +8,26 @@ export default function FeedbackList() {
   const [isLoading, setIsLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState("");
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
-    )
-      .then((res) => {
-        if (!res.ok) throw new Error();
-        return res.json();
-      })
-      .then((data) => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const data = await response.json();
         setFeedbackItems(data.feedbacks);
-      })
-      .catch(() => {
-        // network error, not 2xx response, or JSON parsing error
-        setErrorMessage("Something went wrong.");
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+      } catch (error) {
+        setErrorMessage("Something went wrong. Please try again later.");
+      }
+      setIsLoading(false);
+    };
+    fetchFeedbackItems();
   }, []);
   return (
     <ol className="feedback-list">
